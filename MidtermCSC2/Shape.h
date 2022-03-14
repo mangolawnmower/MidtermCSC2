a197a93e089362d0d6c29c570fab3268fb61b22b
Atticus Rice <mangolawnmower@gmail.com>
2022-03-14 16:10:06 -0500

    Add project files.

---
+++ MidtermCSC2/Shape.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Parent Class Shape
+class shape
+{
+
+
+
+};
+
---
+++ MidtermCSC2/main.cpp
@@ -0,0 +1,10 @@
+#include "Shape.h"
+int main() {
+
+
+
+
+
+
+
+};
